bdf5793e1885d93ab7340e3638222578baa68562
easton <979106585@qq.com>
2023-05-13 12:42:36 +0800

    笔记

---
+++ docs/.vuepress/components/GistDemo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="gist"></div>
+</template>
+
+<script>
+/* eslint-disable no-useless-escape */
+// import postscribe from "postscribe"; //打包会报错，先注释掉 TODO待修复
+export default {
+  mounted: function () {
+    postscribe(
+      "#gist",
+      `<script src="https://gist.github.com/eastonyangxu/a878dcda4b6a0b892039e4458d69ad20.js"><\/script>`
+    );
+  },
+};
+</script>
