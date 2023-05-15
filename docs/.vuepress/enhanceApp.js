34fc94fd33be3c42e204350737058f539c929d56
easton <979106585@qq.com>
2023-05-15 20:02:29 +0800

    引入element-ui

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,4 +1,5 @@
-// import ElementUI from "element-ui";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
@@ -6,5 +7,5 @@ export default ({
   router, // 当前应用的路由实例
   siteData, // 站点元数据
 }) => {
-  // Vue.use(ElementUI);
+  Vue.use(ElementUI);
 };
